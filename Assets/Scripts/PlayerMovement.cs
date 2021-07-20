447e4b25fe3ce95d328e345a14a58c2153063ff2
Grégory Diné <gregorydine86@gmail.com>
2021-07-20 20:28:29 +0200

    upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
