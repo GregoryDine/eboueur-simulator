adb8596d4c4eda93032f29a7efeb6c05c16b52ab
Grégory Diné <gregorydine86@gmail.com>
2021-07-27 11:05:40 +0200

    bug fix

--- Assets/Scripts/DEV.cs
+++ Assets/Scripts/DEV.cs
@@ -4,10 +4,20 @@ using UnityEngine.UI;
 public class DEV : MonoBehaviour
 {
     [SerializeField] Text text;
+    [SerializeField] Transform player;
 
-    private void Awake()
+    void Awake()
     {
         //diplay ver
         text.text = Application.version;
+    }
+
+    void Update()
+    {
+        //tp player back when falling in void
+        if (player.position.y < -10)
+        {
+            player.position = new Vector3(0, 0, 0);
+        }
     }
 }
